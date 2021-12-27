85ef13013ab8b8267a3644780092c4140044129f
gongchunru <gongchunru@gmail.com>
2021-12-27 19:54:05 +0800

    add gee-cache TestRemoveOldest

--- src/gee-cache/day1-lru/geecache/lru/lru_test.go
+++ src/gee-cache/day1-lru/geecache/lru/lru_test.go
@@ -21,3 +21,17 @@ func TestGet(t *testing.T) {
 		t.Fatal("cache miss key=123 failed")
 	}
 }
+
+func TestRemoveOldest(t *testing.T) {
+	k1, k2, k3 := "key1", "key2", "key3"
+	v1, v2, v3 := "value1", "value2", "value3"
+	c := len(k1 + k2 + v1 + v2)
+	lru := New(int64(c), nil)
+	lru.Add(k1, String(v1))
+	lru.Add(k2, String(v2))
+	lru.Add(k3, String(v3))
+
+	if _, ok := lru.Get("key1"); ok || lru.len() != 2 {
+		t.Fatal("RemoveOldest key1 failed")
+	}
+}
