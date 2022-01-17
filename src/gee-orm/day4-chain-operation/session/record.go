d56b8a481d515c5223f7061e4538933bb5def9b4
gongchunru <gongchunru@gmail.com>
2022-01-17 19:27:06 +0800

    add geeorm day4-chain-operation

---
+++ src/gee-orm/day4-chain-operation/session/record.go
@@ -0,0 +1,109 @@
+package session
+
+import (
+	"geeorm/clause"
+	"reflect"
+)
+
+// 记录增删改查相关的代码
+//
+//func (s *Session) Insert(values ...interface{}) (int64, error) {
+//	recordValues := make([]interface{}, 0)
+//	for _, value := range values {
+//		table := s.Model(value).RefTable()
+//		s.clause.Set(clause.INSERT, table.Name, table.FieldNames)
+//		recordValues = append(recordValues, table.RecordValues(value))
+//	}
+//	s.clause.Set(clause.VALUES, recordValues...)
+//	sql, vars := s.clause.Build(clause.INSERT, clause.VALUES)
+//	result, err := s.Raw(sql, vars...).Exec()
+//	if err != nil {
+//		return 0, err
+//	}
+//	return result.RowsAffected()
+//}
+
+// Insert one or more records in database
+func (s *Session) Insert(values ...interface{}) (int64, error) {
+	recordValues := make([]interface{}, 0)
+	for _, value := range values {
+		table := s.Model(value).RefTable()
+		s.clause.Set(clause.INSERT, table.Name, table.FieldNames)
+		recordValues = append(recordValues, table.RecordValues(value))
+	}
+
+	s.clause.Set(clause.VALUES, recordValues...)
+	sql, vars := s.clause.Build(clause.INSERT, clause.VALUES)
+	result, err := s.Raw(sql, vars...).Exec()
+	if err != nil {
+		return 0, err
+	}
+
+	return result.RowsAffected()
+}
+
+func (s *Session) Find(values interface{}) error {
+	destSlice := reflect.Indirect(reflect.ValueOf(values))
+	// 获取切片的单个元素的类型 destType
+	destType := destSlice.Type().Elem()
+	table := s.Model(reflect.New(destType).Elem().Interface()).RefTable()
+
+	s.clause.Set(clause.SELECT, table.Name, table.FieldNames)
+
+	sql, vars := s.clause.Build(clause.SELECT, clause.WHERE, clause.ORDERBY, clause.LIMIT)
+	rows, err := s.Raw(sql, vars...).QueryRows()
+	if err != nil {
+		return err
+	}
+
+	for rows.Next() {
+		// 利用反射创建 destType 的实例 dest
+		dest := reflect.New(destType).Elem()
+		var values []interface{}
+		for _, name := range table.FieldNames {
+			// 将 dest 的所有字段平铺开，构造切片 values。
+			values = append(values, dest.FieldByName(name).Addr().Interface())
+		}
+		if err := rows.Scan(values...); err != nil {
+			return err
+		}
+		destSlice.Set(reflect.Append(destSlice, dest))
+	}
+	return rows.Close()
+}
+func (s *Session) Update(kv ...interface{}) (int64, error) {
+	m, ok := kv[0].(map[string]interface{})
+	if !ok {
+		m = make(map[string]interface{})
+		for i := 0; i < len(kv); i += 2 {
+			m[kv[i].(string)] = kv[i+1]
+		}
+	}
+	s.clause.Set(clause.UPDATE, s.RefTable().Name, m)
+	sql, vars := s.clause.Build(clause.UPDATE, clause.WHERE)
+	result, err := s.Raw(sql, vars...).Exec()
+	if err != nil {
+		return 0, err
+	}
+	return result.RowsAffected()
+}
+
+func (s *Session) Delete() (int64, error) {
+	s.clause.Set(clause.DELETE, s.RefTable().Name)
+	sql, vars := s.clause.Build(clause.DELETE, clause.WHERE)
+	result, err := s.Raw(sql, vars...).Exec()
+	if err != nil {
+		return 0, err
+	}
+	return result.RowsAffected()
+}
+
+func (s *Session) Count() (int64, error) {
+	s.clause.Set(clause.COUNT, s.RefTable().Name)
+	sql, vars := s.clause.Build(clause.COUNT, clause.WHERE)
+	result, err := s.Raw(sql, vars...).Exec()
+	if err != nil {
+		return 0, err
+	}
+	return result.RowsAffected()
+}
