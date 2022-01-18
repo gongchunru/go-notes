a503329195cfcb2cc9d603a527fbb3ab89b6b8e1
gongchunru <gongchunru@gmail.com>
2022-01-18 18:16:54 +0800

    add day7 migrate test

---
+++ src/gee-orm/day7-migrate/geeorm_test.go
@@ -0,0 +1,89 @@
+package geeorm
+
+import (
+	"errors"
+	"geeorm/session"
+	_ "github.com/mattn/go-sqlite3"
+	"reflect"
+	"testing"
+)
+
+func OpenDB(t *testing.T) *Engine {
+	t.Helper()
+	engine, err := NewEngine("sqlite3", "gee.db")
+	if err != nil {
+		t.Fatal("failed to connect", err)
+	}
+	return engine
+}
+
+type User struct {
+	Name string `geeorm:"PRIMARY KEY"`
+	Age  int
+}
+
+func TestEngine_Transaction(t *testing.T) {
+	t.Run("rolllback", func(t *testing.T) {
+		transactionRollback(t)
+	})
+	t.Run("commit", func(t *testing.T) {
+		transactionCommit(t)
+	})
+}
+
+func transactionCommit(t *testing.T) {
+	engine := OpenDB(t)
+	defer engine.Close()
+	s := engine.NewSession()
+	_ = s.Model(&User{}).DropTable()
+	_, err := engine.Transaction(func(s *session.Session) (result interface{}, err error) {
+		_ = s.Model(&User{}).CreateTable()
+		_, err = s.Insert(&User{"Tom", 18})
+		return
+	})
+
+	u := &User{}
+	_ = s.First(u)
+	if err != nil || u.Name != "Tom" {
+		t.Fatal("failed to commit")
+	}
+}
+
+func transactionRollback(t *testing.T) {
+	engine := OpenDB(t)
+	defer engine.Close()
+	s := engine.NewSession()
+	_ = s.Model(&User{}).DropTable()
+	_, err := engine.Transaction(func(s *session.Session) (result interface{}, err error) {
+		_ = s.Model(&User{}).CreateTable()
+		_, err = s.Insert(&User{"Tom", 18})
+		return nil, errors.New("Error")
+	})
+
+	u := &User{}
+	_ = s.First(u)
+	if err != nil || u.Name != "Tom" {
+		t.Fatal("failed to rollback")
+	}
+}
+
+func TestNewEngine(t *testing.T) {
+	engine := OpenDB(t)
+	defer engine.Close()
+}
+
+func TestEngine_Migrate(t *testing.T) {
+	engine := OpenDB(t)
+	defer engine.Close()
+	s := engine.NewSession()
+	_, _ = s.Raw("DROP TABLE IF EXISTS User;").Exec()
+	_, _ = s.Raw("CREATE TABLE User(Name text PRIMARY KEY, xxx integer);").Exec()
+	_, _ = s.Raw("INSERT INTO User(`Name`) VALUES (?),(?)", "Tom", "Sam").Exec()
+
+	engine.Migrate(&User{})
+	rows, _ := s.Raw("SELECT * FROM User").QueryRows()
+	columns, _ := rows.Columns()
+	if !reflect.DeepEqual(columns, []string{"Name", "Age"}) {
+		t.Fatal("Failed to migrate table User, got columns", columns)
+	}
+}
