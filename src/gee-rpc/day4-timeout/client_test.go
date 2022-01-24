e7b8a7591eb7f17198450a2bf7589d04f0715290
gongchunru <gongchunru@gmail.com>
2022-01-24 22:32:32 +0800

    add geerpc2 day4 test

--- src/gee-rpc/day4-timeout/client_test.go
+++ src/gee-rpc/day4-timeout/client_test.go
@@ -47,3 +47,27 @@ func TestClient_DialTimeout(t *testing.T) {
 		_assert(err != nil && strings.Contains(err.Error(), "handle timeout"), "expect a timeout error")
 	})
 }
+func TestClient_Call(t *testing.T) {
+	t.Parallel()
+	addrCh := make(chan string)
+	go startServer(addrCh)
+
+	addr := <-addrCh
+	time.Sleep(time.Second)
+	t.Run("client timeout", func(t *testing.T) {
+		client, _ := Dial("tcp", addr)
+		ctx, _ := context.WithTimeout(context.Background(), time.Second)
+		var reply int
+		err := client.Call(ctx, "Bar.Timeout", 1, &reply)
+		_assert(err != nil && strings.Contains(err.Error(), ctx.Err().Error()), "expect a timeout error")
+	})
+
+	t.Run("server handle timeout", func(t *testing.T) {
+		client, _ := Dial("tcp", addr, &Option{
+			HandlerTimeout: time.Second,
+		})
+		var reply int
+		err := client.Call(context.Background(), "Bar.Timeout", 1, &reply)
+		_assert(err != nil && strings.Contains(err.Error(), "handle timeout"), "expect a timeout error")
+	})
+}
